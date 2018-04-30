d47551a4e7f439f9b88965343c158853daf86379
vilkku89 <36002316+vilkku89@users.noreply.github.com>
2018-04-30 13:18:43 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,16 +1,22 @@
-# moduulit joita prosessi tällä hetkellä käyttää -> jatkossa luultavasti Scrapy
 import requests
 from lxml import html
 
-link_collection = []
+link_path = open("filepath")
+read_links = link_path.read()
+linklist = read_links.split(",")
 
-# haetaan esim 100 sivua artikkeleita jotka sisältävät hakusanan
-for i in range(1, 101):
-    url = "https://asia.nikkei.com/search/find?search_keyword=HAKUSANA&page=" + str(i)
-    page = requests.get(url)
+base_url = "https://asia.nikkei.com"
+full_url = []
+
+for i in linklist:
+    url = base_url + i
+    full_url.append(url)
+
+read_body = []
+
+# otetaan pelkkä body text jokaisen otsikon takaa löytyvästä artikkelista
+for i in full_url:
+    page = requests.get(i)
     tree = html.fromstring(page.content)
-    
-    # polku otsikon hypertekstiin
-    a = tree.xpath('//div[@class="article-box"]//h2/a/@href')
-    
-    link_collection.append(a)
+    body_class = tree.xpath('//div[@class="articleBodyText"]//p/text()')
+    read_body.append(body_class)
