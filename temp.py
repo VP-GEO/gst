84b499e9d3e9621b4951da52e48c8917bbc36ce9
vilkku89 <36002316+vilkku89@users.noreply.github.com>
2018-04-30 13:24:07 +0300

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,24 +1,3 @@
 import requests
 from lxml import html
 
-# luetaan äsken tallenetut linkit listana
-link_path = open("filepath")
-read_links = link_path.read()
-linklist = read_links.split(",")
-
-# linkit relatiivisia joten täydennetään URL
-base_url = "https://asia.nikkei.com"
-full_url = []
-
-for i in linklist:
-    url = base_url + i
-    full_url.append(url)
-
-read_body = []
-
-# otetaan pelkkä body text jokaisen otsikon takaa löytyvästä artikkelista
-for i in full_url:
-    page = requests.get(i)
-    tree = html.fromstring(page.content)
-    body_class = tree.xpath('//div[@class="articleBodyText"]//p/text()')
-    read_body.append(body_class)
